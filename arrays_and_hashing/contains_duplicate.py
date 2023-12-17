b755ebc48ccc4ea845d3885b1b11570bad8696f8
miltonhuynh <miltonhuynh@gmail.com>
2023-12-17 10:44:42 -0800

    1

---
+++ arrays_and_hashing/contains_duplicate.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        # Initializes a set to store unique numbers, uses a set instead of an array because adding is O(1) instead of O(N).
+        nums_set = set()
+        # Iterates through input array of numbers.
+        for n in nums:
+            # Number already found in set, return True.
+            if n in nums_set:
+                return True
+            # Number not in set, add it to the set.
+            else:
+                nums_set.add(n)
+        # Finished iterating through array, duplicate was never found.
+        return False
+
