5aab629c3d87885ea0303a972e43fd6d9cbd260a
miltonhuynh <miltonhuynh@gmail.com>
2023-12-19 19:52:15 -0800

    a

--- arrays_and_hashing/valid_anagram.py
+++ arrays_and_hashing/valid_anagram.py
@@ -1,25 +1,21 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        s_set = set()
-        t_set = set()
-
-        for i in s:
-            s_set.add(i)
-        for i in t:
-            t_set.add(i)
-
-        print(s_set)
-        print(t_set)
-
-        if s_set == t_set:
-            return True
-
-        else:
+        # Two words must be the same length to be an anagram.
+        if len(s) != len(t):
             return False
 
+        # Initializes an empty dictionary for both words.
+        countS = {}
+        countT = {}
 
-test = Solution()
-s = ["cat"]
-t = ["tac"]
-print(test.isAnagram(s,t))
-
+        # Iterates through the two words, adding each unique letter to dictionary or updating occurrences.
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countS[t[i]] = 1 + countS.get(t[i], 0)
+        # Iterates through dictionaries and checks if both have the same count for each letter.
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                # False returned if difference found in dictionaries.
+                return False
+        # Both dictionaries iterated through, return True if end reached.
+        return True
