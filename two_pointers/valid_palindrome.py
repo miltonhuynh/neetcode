154baebffde336e47d7f2cbda3b02c255ec35119
miltonhuynh <miltonhuynh@gmail.com>
2023-12-28 11:14:04 -0800

    a

--- two_pointers/valid_palindrome.py
+++ two_pointers/valid_palindrome.py
@@ -1,1 +1,20 @@
-# placeholder
+"""
+125. Valid Palindrome (Easy)
+https://leetcode.com/problems/valid-palindrome
+"""
+
+"""
+Not optimal solution, uses built-in functions and extra memory.
+"""
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Initializes a new empty string to store new string with only alphanumeric characters
+        newStr = ""
+
+        # Iterates through input string and adds to new string if character is alphanumeric.
+        for i in s:
+            if i.isalnum():
+                # Ignores cases
+                newStr += i.lower()
+        # Returns true if new string and its reverse are equivalent
+        return newStr == newStr[::-1]
