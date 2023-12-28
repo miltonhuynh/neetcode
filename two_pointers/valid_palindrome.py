864497c2fe20a4c45dd7f679cbd8b23cdb8f5bf5
miltonhuynh <miltonhuynh@gmail.com>
2023-12-28 11:58:20 -0800

    a

--- two_pointers/valid_palindrome.py
+++ two_pointers/valid_palindrome.py
@@ -18,3 +18,34 @@ class Solution:
                 newStr += i.lower()
         # Returns true if new string and its reverse are equivalent
         return newStr == newStr[::-1]
+
+
+"""
+Optimal solution, does not use extra memory.
+"""
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Converts string to all capital letters to ignore case.
+        s = s.upper()
+        # Initializes pointers at beginning and end of string.
+        left = 0
+        right = len(s) - 1
+        # Runs until 2 pointers meet in the middle
+        while left <= right:
+            # If left or right pointer is not alphanumeric, move onto the next
+            if s[left].isalnum() == False:
+                left += 1
+                # Continue prevents comparison from being done.
+                continue
+            if s[right].isalnum() == False:
+                right -= 1
+                continue
+            if s[left] != s[right]:
+                return False
+            # Compares the next 2 pointers
+            left += 1
+            right -= 1
+        # No mismatch found, returns True.
+        return True
